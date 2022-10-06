2cc4e440af4e805cb59f54df9bbce8b0e3b19e5e
YukinoKatagiri <115126048+YukinoKatagiri@users.noreply.github.com>
2022-10-06 09:02:42 +0700

    Update Green.sh

--- Green.sh
+++ Green.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for n in {1..3600};
 do
+    git add .
     git config --local user.email "115126048+YukinoKatagiri@users.noreply.github.com"
     git config --local user.name "YukinoKatagiri"
     git config --local rebase.autoStash true
